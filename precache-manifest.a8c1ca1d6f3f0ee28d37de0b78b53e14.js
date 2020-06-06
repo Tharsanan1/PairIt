36b8ddef1fd9003c920e67321694c525506611aa
tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2020-06-06 23:35:28 +0530

    Updates

---
+++ precache-manifest.a8c1ca1d6f3f0ee28d37de0b78b53e14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ccbd753a4466083b9251dcdf50d5638",
+    "url": "/Tharsanan1/PairIt/index.html"
+  },
+  {
+    "revision": "4aea13a118ad1b810fb5",
+    "url": "/Tharsanan1/PairIt/static/js/2.cb023848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tharsanan1/PairIt/static/js/2.cb023848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3f27c37205d9a16747",
+    "url": "/Tharsanan1/PairIt/static/js/main.f867929c.chunk.js"
+  },
+  {
+    "revision": "9619f54f2317d0a6935c",
+    "url": "/Tharsanan1/PairIt/static/js/runtime-main.147aae31.js"
+  }
+]);
